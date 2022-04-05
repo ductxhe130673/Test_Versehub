05ccd5b86b2e81d03d835ae4338d30aa659bee89
ductxhe130673 <ductxhe130673@fpt.edu.vn>
2022-04-05 20:34:56 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import HorirontalSlideShow from "./component/HorirontalSlideShow";
+
+
+
+function App() {
+
+  return <HorirontalSlideShow showNumber={5} />;
+
+}
+
+
+
+export default App;
